813ad064945b401644edd97391dd52788df57610
DiazKun <68154504+DiazKun@users.noreply.github.com>
2022-11-26 11:05:31 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+/* 
+ -- READ THIS --
+
+- You can change the server feature in index.html file and find the feature part / feature area
+- You can change your server about in index.html file and find the about part / about us area
+
+ -- CHANGE THE WEBSITE OPTION IN BELOW --
+*/
+let option = {
+    server_name: "GrowDW",
+    server_description: "This server since 2019.",
+    owner: [
+        "DiazArmn",
+        "Wibi"
+    ],
+    discord_invite_link: "https://chat.whatsapp.com/DV2cUZqkVOTKUmlRqifruE",
+    ip_address: "172.104.59.126",
+    host_for_android: "https://cdn.discordapp.com/attachments/968130979745132594/1046011279288049784/GrowDW.txt"
+}
+
